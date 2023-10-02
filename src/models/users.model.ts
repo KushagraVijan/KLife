3a29394b1f8f30d9c485f529ebfb204e6e436f1c
KushagraV <Kushagra_Vijan@ultimatesoftware.com>
2023-10-02 13:57:58 +0530

    Images added

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -25,16 +25,18 @@ export class Users {
             new User('divu', 'Divye Chopra', 'm', 'Male BFF', 'Saharanpur', '20th November', true, true, false),
             new User('anshiii', 'Anshika Saini', 'f', 'Female BFF', 'Saharanpur', '17th November', true, true, false),
             new User('kush', 'Kushagra', 'm', 'Myself', 'Saharanpur', 'Secret Day', true, true, false),
-            new User('sam', 'Sanyam Jain', 'm', 'Male BFF', 'Saharanpur', '26th November', false, true, false),
+            new User('sam', 'Sanyam Jain', 'm', 'Male BFF', 'Saharanpur', '26th November', true, true, false),
             new User('megha', 'Palak Srivastava', 'f', 'Friend', 'Roorkee', '21st May', true, true, false),
             new User('saira', 'Sairas Arora', 'm', 'Male BFF', 'Faridkot', '16th September', true, true, false),
             new User('@dmink', '',  'm', '', '', '', false, false, true),
             new User('shivu', 'Shivangi Kaushik', 'f', 'Friend', 'Saharanpur', '21st November', true, true, false),
-            new User('panda', 'Anshiii', 'f', 'My sweet and hot NTGF', 'Saharanpur', '17th November', false, false, false),
+            new User('panda', 'Anshiii', 'f', 'My sweet and hot NTGF', 'Saharanpur', '17th November', true, false, false),
             new User('sparsh', 'Sparsh Goyal', 'm', 'Close Friend', 'Bathinda', '28th June', true, true, false),
             new User('pratik', 'Pratik Asija', 'm', 'Flatmate', 'Rajpura', '1st December', true, true, false),
             new User('varun', 'Varun Bansal', 'm', 'Friend', 'Sangrur', '27th July', true, true, false),
-            new User('himanchi', 'Himanshi Garg', 'f', 'Friend', 'Faridkot', '3rd October', true, true, false)
+            new User('aman', 'Aman Jain', 'm', 'Friend', 'Jaipur', '5th August', true, true, false),
+            new User('himanchi', 'Himanshi Garg', 'f', 'Friend', 'Faridkot', '3rd October', true, true, false),
+            new User('akshit', 'Akshit Jain', 'm', 'Friend', 'Saharanpur', '29th October', true, true, false)
         ];
     }
 }
